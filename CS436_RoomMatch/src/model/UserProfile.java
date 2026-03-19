350fd4bb22d0b1abb6651ce2efc7dd2232a7aa1b
mayaelzein <148112011+mayaelzein@users.noreply.github.com>
2026-03-19 11:00:08 -0700

    Add lifestyle preference fields to UserProfile (sleep, cleanliness, guests)
    Added preference attributes and access methods to support compatibility matching logic

--- CS436_RoomMatch/src/model/UserProfile.java
+++ CS436_RoomMatch/src/model/UserProfile.java
@@ -3,6 +3,32 @@ package model;
 public class UserProfile extends Observable {
 	private boolean loggedIn = false;
 	private String username;
+	// prevent null issues
+	private String sleepSchedule = "";
+	private String cleanliness = "";
+	private String guests = "";
+	
+	public void setPreferences(String sleep, String clean, String guest) {
+	    sleepSchedule = sleep;
+	    cleanliness = clean;
+	    guests = guest;
+	}
+	
+	public String getSleepSchedule() {
+		return sleepSchedule;
+	}
+	
+	public String getCleanliness() {
+		return cleanliness;
+	}
+	
+	public String getGuests() {
+		return guests;
+	}
+
+	public String getUsername() {
+	    return username;
+	}
 	
 	public void login(String user) {
 		if( loggedIn ) {
@@ -26,5 +52,16 @@ public class UserProfile extends Observable {
 			return username;
 		
 		return null;
+	}
+
+	// for debugging
+	@Override
+	public String toString() {
+	    return "UserProfile{" +
+	            "username='" + username + '\'' +
+	            ", sleep='" + sleepSchedule + '\'' +
+	            ", cleanliness='" + cleanliness + '\'' +
+	            ", guests='" + guests + '\'' +
+	            '}';
 	}
 }
